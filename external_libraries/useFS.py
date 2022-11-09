f37d4964e115e5b6bf788f5ceafce088cc8f00f9
Chris Clarke <christopher_clarke@mymail.sutd.edu.sg>
2022-11-09 21:46:26 +0800

    commit

---
+++ external_libraries/useFS.py
@@ -0,0 +1,18 @@
+from FS.ba import jfs as bajfs
+from FS.cs import jfs as csjfs
+from FS.fa import jfs as fajfs
+from FS.fpa import jfs as fpajfs
+from FS.ga import jfs as gajfs
+from FS.gwo import jfs as gwojfs
+from FS.hho import jfs as hhojfs
+from FS.ja import jfs as jajfs
+from FS.pso import jfs as psojfs
+from FS.sca import jfs as scajfs
+from FS.ssa import jfs as ssajfs
+from FS.woa import jfs as woajfs
+
+class useFS:
+    def __init__(self):
+        
+        
+
